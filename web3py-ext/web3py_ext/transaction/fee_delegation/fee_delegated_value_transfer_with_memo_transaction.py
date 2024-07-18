e0f69756c62868f221ca9606fd7248a877276f5c
kjeom <jack.jin@klaytn.foundation>
2024-07-18 22:42:01 +0900

    fix bugs

--- web3py-ext/web3py_ext/transaction/fee_delegation/fee_delegated_value_transfer_with_memo_transaction.py
+++ web3py-ext/web3py_ext/transaction/fee_delegation/fee_delegated_value_transfer_with_memo_transaction.py
@@ -332,6 +332,8 @@ class FeeDelegatedValueTransferWithMemoTransaction(_TypedTransactionImplementati
             transaction_with_signatures = merge(transaction_without_signature_fields, {'signatures':[]})
         
         if vrs != {} and vrs not in transaction_with_signatures['signatures']:
+            if type(transaction_with_signatures['signatures']) is not list:
+                transaction_with_signatures['signatures'] = list(transaction_with_signatures['signatures'])
             transaction_with_signatures['signatures'].append(vrs)
         
         transaction_with_signatures = pipe(

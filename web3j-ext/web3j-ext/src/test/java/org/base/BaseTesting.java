59719758949274c053ed3c34e4b80c81319459a0
Sotatek-TanHoang <tan.hoang@sotatek.com>
2024-09-19 17:30:13 +0700

    feat: unit-test for web3j-ext

--- web3j-ext/web3j-ext/src/test/java/org/base/BaseTesting.java
+++ web3j-ext/web3j-ext/src/test/java/org/base/BaseTesting.java
@@ -1,18 +1,28 @@
 package org.base;
 
+import org.mockito.Mockito;
 import org.web3j.crypto.KaiaCredentials;
 import org.web3j.example.keySample;
+import org.web3j.protocol.Web3jService;
 import org.web3j.protocol.http.HttpService;
 import org.web3j.protocol.kaia.Web3j;
 
+
+
 public abstract class BaseTesting {
+    private final Web3jService web3jService=Mockito.mock(HttpService.class);
+    private final Web3j mockedWeb3j = Web3j.build(web3jService);
     private final Web3j web3j = Web3j.build(new HttpService(keySample.BAOBAB_URL));
     private final KaiaCredentials credentials = KaiaCredentials.create(keySample.LEGACY_KEY_privkey);
-
+    public Web3j getMockedWeb3j() {
+        return mockedWeb3j;
+    }
     public Web3j getWeb3j() {
         return web3j;
     }
-
+    public Web3jService getWeb3jService() {
+        return web3jService;
+    }
     public KaiaCredentials getCredentials() {
         return credentials;
     }
--- web3j-ext/web3j-ext/src/test/java/org/signer/SignerTest.java
+++ web3j-ext/web3j-ext/src/test/java/org/signer/SignerTest.java
@@ -3,8 +3,16 @@ package org.signer;
 import org.base.BaseTesting;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.web3j.crypto.KaiaSignatureData;
-import org.web3j.crypto.Sign;
+import org.web3j.crypto.*;
+import org.web3j.crypto.transaction.type.TxType;
+import org.web3j.example.keySample;
+import org.web3j.protocol.core.DefaultBlockParameterName;
+import org.web3j.protocol.core.methods.response.EthChainId;
+import org.web3j.protocol.core.methods.response.EthSendTransaction;
+import org.web3j.utils.Numeric;
+
+import java.io.IOException;
+import java.math.BigInteger;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,4 +28,147 @@ public class SignerTest extends BaseTesting {
 
         assertNotNull(result);
     }
+
+    @Test
+    @DisplayName("test signTransaction")
+    void testSignTransaction() throws IOException {
+        KaiaCredentials credentials = KaiaCredentials.create(keySample.LEGACY_KEY_privkey);
+
+        BigInteger GAS_PRICE = BigInteger.valueOf(50000000000L);
+        BigInteger GAS_LIMIT = BigInteger.valueOf(6721950);
+        String from = credentials.getAddress();
+        EthChainId EthchainId = this.getWeb3j().ethChainId().send();
+        long chainId = EthchainId.getChainId().longValue();
+        String to = "0x000000000000000000000000000000000000dead";
+        BigInteger nonce = this.getWeb3j().ethGetTransactionCount(from, DefaultBlockParameterName.LATEST).send()
+                .getTransactionCount();
+        BigInteger value = BigInteger.valueOf(100);
+
+        TxType.Type type = TxType.Type.VALUE_TRANSFER;
+
+        KaiaRawTransaction raw = KaiaRawTransaction.createTransaction(
+                type,
+                nonce,
+                GAS_PRICE,
+                GAS_LIMIT,
+                to,
+                value,
+                from);
+
+        byte[] signedMessage = KaiaTransactionEncoder.signMessage(raw, chainId, credentials);
+        String hexValue = Numeric.toHexString(signedMessage);
+
+        assertNotNull(hexValue);
+    }
+    @Test
+    @DisplayName("test signTransactionAsFeePayer")
+    void testSignTransactionAsFeePayer() throws IOException {
+        KaiaCredentials credentials = KaiaCredentials.create(keySample.LEGACY_KEY_privkey);
+        KaiaCredentials credentials_feepayer = KaiaCredentials.create(keySample.LEGACY_KEY_FEEPAYER_privkey);
+
+        BigInteger GAS_PRICE = BigInteger.valueOf(50000000000L);
+        BigInteger GAS_LIMIT = BigInteger.valueOf(6721950);
+        String from = credentials.getAddress();
+        EthChainId EthchainId = this.getWeb3j().ethChainId().send();
+        long chainId = EthchainId.getChainId().longValue();
+        String to = "0x000000000000000000000000000000000000dead";
+        BigInteger nonce = this.getWeb3j().ethGetTransactionCount(from, DefaultBlockParameterName.LATEST).send()
+                .getTransactionCount();
+        BigInteger value = BigInteger.valueOf(100);
+
+        TxType.Type type = TxType.Type.FEE_DELEGATED_VALUE_TRANSFER;
+
+        KaiaRawTransaction raw = KaiaRawTransaction.createTransaction(
+                type,
+                nonce,
+                GAS_PRICE,
+                GAS_LIMIT,
+                to,
+                value,
+                from);
+
+        // Sign as sender
+        byte[] signedMessage = KaiaTransactionEncoder.signMessage(raw, chainId, credentials);
+
+        // Sign same message as Fee payer
+        signedMessage = KaiaTransactionEncoder.signMessageAsFeePayer(raw, chainId, credentials_feepayer);
+
+        String hexValue = Numeric.toHexString(signedMessage);
+
+        assertNotNull(hexValue);
+    }
+    @Test
+    @DisplayName("test sendTransaction")
+    void testSendTransaction() throws IOException {
+        KaiaCredentials credentials = KaiaCredentials.create(keySample.LEGACY_KEY_privkey);
+
+        BigInteger GAS_PRICE = BigInteger.valueOf(50000000000L);
+        BigInteger GAS_LIMIT = BigInteger.valueOf(6721950);
+        String from = credentials.getAddress();
+        EthChainId EthchainId = this.getWeb3j().ethChainId().send();
+        long chainId = EthchainId.getChainId().longValue();
+        String to = "0x000000000000000000000000000000000000dead";
+        BigInteger nonce = this.getWeb3j().ethGetTransactionCount(from, DefaultBlockParameterName.LATEST).send()
+                .getTransactionCount();
+        BigInteger value = BigInteger.valueOf(100);
+
+        TxType.Type type = TxType.Type.VALUE_TRANSFER;
+
+        KaiaRawTransaction raw = KaiaRawTransaction.createTransaction(
+                type,
+                nonce,
+                GAS_PRICE,
+                GAS_LIMIT,
+                to,
+                value,
+                from);
+
+        byte[] signedMessage = KaiaTransactionEncoder.signMessage(raw, chainId, credentials);
+        String hexValue = Numeric.toHexString(signedMessage);
+        EthSendTransaction transactionResponse = this.getWeb3j().ethSendRawTransaction(hexValue).send();
+        String txHash = transactionResponse.getResult();
+        assertNotNull(txHash);
+    }
+    @Test
+    @DisplayName("test sendTransactionAsFeePayer")
+    void testSendTransactionAsFeePayer() throws IOException {
+        KaiaCredentials credentials = KaiaCredentials.create(keySample.LEGACY_KEY_privkey);
+        KaiaCredentials credentials_feepayer = KaiaCredentials.create(keySample.LEGACY_KEY_FEEPAYER_privkey);
+
+        BigInteger GAS_PRICE = BigInteger.valueOf(50000000000L);
+        BigInteger GAS_LIMIT = BigInteger.valueOf(6721950);
+        String from = credentials.getAddress();
+        EthChainId EthchainId = this.getWeb3j().ethChainId().send();
+        long chainId = EthchainId.getChainId().longValue();
+        String to = "0x000000000000000000000000000000000000dead";
+        BigInteger nonce = this.getWeb3j().ethGetTransactionCount(from, DefaultBlockParameterName.LATEST).send()
+                .getTransactionCount();
+        BigInteger value = BigInteger.valueOf(100);
+
+        TxType.Type type = TxType.Type.FEE_DELEGATED_VALUE_TRANSFER;
+
+        KaiaRawTransaction raw = KaiaRawTransaction.createTransaction(
+                type,
+                nonce,
+                GAS_PRICE,
+                GAS_LIMIT,
+                to,
+                value,
+                from);
+
+        // Sign as sender
+        byte[] signedMessage = KaiaTransactionEncoder.signMessage(raw, chainId, credentials);
+
+        // Sign same message as Fee payer
+        signedMessage = KaiaTransactionEncoder.signMessageAsFeePayer(raw, chainId, credentials_feepayer);
+
+        String hexValue = Numeric.toHexString(signedMessage);
+
+        EthSendTransaction transactionResponse = this.getWeb3j().ethSendRawTransaction(hexValue).send();
+        String txHash = transactionResponse.getResult();
+        assertNotNull(txHash);
+    }
+
+
 }
+

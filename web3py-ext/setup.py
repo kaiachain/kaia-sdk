9545461e7a7c3e175302f9163d913f80eb61fc9d
praveen-kaia <praveen.challa@kaia.io>
2025-07-23 17:37:18 +0800

    Bumped all versions to v2.0.9

--- web3py-ext/setup.py
+++ web3py-ext/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import pathlib
 
 NAME = "web3py_ext"
-VERSION = "2.0.8"
+VERSION = "2.0.9"
 
 with open("./README.md") as readme:
     long_description = readme.read()
